62e55b8918f8afbad7a0f8a82d35e36a87cf8a7d
Taylor King <dekkofilms@gmail.com>
2016-12-12 14:06:55 -0600

    Initial Commit

---
+++ lstr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  lstr
+//
+//  Created by Taylor King on 12/12/16.
+//  Copyright © 2016 Taylor King. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
